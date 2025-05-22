9c783df000b0b29037d5338515cfb12839dc6693
Serenafrisman <49008981@est.ort.edu.ar>
2025-05-22 10:53:38 -0300

    Ejercicio colisiones

---
+++ Assets/Scripts/DetecciondeColiciones.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetecciondeColiciones : MonoBehaviour
+
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+        if (col.gameObject.name == "Player") 
+        {
+            Destroy(gameObject);
+        }
+      
+    }
+}
