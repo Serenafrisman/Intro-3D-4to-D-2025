4d9ea8f2cb2d2705e9f7d4086f3de19a3a67d496
Serenafrisman <49008981@est.ort.edu.ar>
2025-05-26 10:46:34 -0300

    terminado ejercico de scorepoints

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+   
+    public int score;
+    public UIManager uiManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+   public void AddScore ()
+    {
+        score++;
+        uiManager.UpdateScore(score);
+    }
+
+    public void AddScore(int scorePoints)
+    {
+        score += scorePoints;
+        uiManager.UpdateScore(score);
+    }
+}
