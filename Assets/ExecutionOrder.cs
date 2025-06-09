36bb6acb50564ca11a2e637108154d744b3cf786
Serenafrisman <49008981@est.ort.edu.ar>
2025-06-09 10:43:36 -0300

    Terminar los errores

---
+++ Assets/ExecutionOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecutionOrder : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+    void FixedUdapte()
+    {
+        Debug.Log("FixedUdapte");
+    }
+    void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+}
